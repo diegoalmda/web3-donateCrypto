a8e9669eea13bcc77ce356e59d1f4375723c2fb3
Diego Almeida <diegoalmda@gmail.com>
2023-04-12 15:15:54 -0300

    chore: created home page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export function Footer() {
+  const newDate = new Date()
+  
+  return (
+    <footer className="container px-4 py-3 d-flex flex-wrap justify-content-between align-items-center my-4 border-top">
+      <p className="col-md-4 mb-0 text-body-secondary">
+        &copy; {newDate.getFullYear()} Donate crypto, Inc.
+      </p>
+      <ul className="nav col-md-4 justify-content-end">
+        <li className="nav-item">
+          <Link href="/" className="nav-link px-2 text-body-primary">Início</Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/create" className="nav-link px-2 text-body-primary">Nova Campanha</Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/donate" className="nav-link px-2 text-body-primary">Doar</Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/about" className="nav-link px-2 text-body-primary">Sobre</Link>
+        </li>
+      </ul>
+    </footer>
+  )
+}
